2396374affabf97316695f5949d5b411c064a98f
Ajay Gautam <ajay610@gmail.com>
2020-12-03 20:58:07 -0500

    add from old project

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+# bandnames_firebase_flutter
+
+Flutter application to explore firebase connectivity
+
+Project as of Dec 3, 2020.
+
+Inspired from https://www.youtube.com/watch?v=DqJ_KjFzL9I
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -115,3 +115,75 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Band name survey for Tequila drinkers',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Possible tequila band names'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot doc) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(child: Text(doc['name'])),
+          Container(
+            decoration: const BoxDecoration(color: Colors.red),
+            padding: const EdgeInsets.all(10),
+            child: Text(doc['votes '].toString()),
+          )
+        ],
+      ),
+      onTap: () {
+        print("increase votes here");
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("bandnames").snapshots(),
+        builder: (context, snapshot) {
+          if (! snapshot.hasData) {
+            return const Text("Loading...");
+          }
+          return ListView.builder(
+            itemExtent: 80,
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index]),
+          );
+        }
+      ),
+     );
+  }
+}
